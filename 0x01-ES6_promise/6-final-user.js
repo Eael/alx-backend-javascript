866a5e6068c29fb2cf720f64706774c6bb3c2cc2
Eael <eaelspagad@outlook.com>
2024-05-29 21:40:10 +0000

    666

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,7 +1,7 @@
 import signUpUser from './4-promises.js';
 import uploadPhoto from './5-promises.js';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((results) => (
