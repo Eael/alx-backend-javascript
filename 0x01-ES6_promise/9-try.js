c8cc6ebbf2dc7394c9b434ccf5f18d397e45efa6
Eael <eaelspagad@outlook.com>
2024-05-30 10:39:50 +0000

    lint 2

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,8 +1,13 @@
 export default function guardrail(mathFunction) {
   const queue = [];
-  try { queue.push(mathFunction()); }
-  catch (error) { queue.push(String(error)); }
-  finally { queue.push('Guardrail was processed'); }
-  
+  try {
+    queue.push(mathFunction());
+  }
+  catch (error) {
+    queue.push(String(error));
+   }
+  finally {
+    queue.push('Guardrail was processed');
+  }
   return queue;
 }
