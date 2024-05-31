3f8ec69e5beff0a6476f9ac4e1587bfcd3d2769e
Eael <eaelspagad@outlook.com>
2024-05-31 10:12:05 +0000

    linting 2-hbtn_course

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -23,11 +23,33 @@ export default class HorbeltonCourse {
   }
 
   /**
+   * Sets the name of the course.
+   * @param {string} name - The name of the course.
+   */
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('name must be a string');
+    }
+    this._name = name;
+  }
+
+  /**
    * Gets the length of the course.
    * @returns {number} The length of the course.
    */
   get length() {
     return this._length;
+  }
+
+  /**
+   * Sets the length of the course.
+   * @param {number} length - The length of the course.
+   */
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw new TypeError('length must be a number');
+    }
+    this._length = length;
   }
 
   /**
@@ -39,36 +61,14 @@ export default class HorbeltonCourse {
   }
 
   /**
-   * Sets the name of the course.
-   * @param {string} name - The name of the course.
-   */
-  set name(value) {
-    if (typeof value !== 'string') {
-      throw new TypeError('name must be a string');
-    }
-    this._name = value;
-  }
-
-  /**
-   * Sets the length of the course.
-   * @param {number} length - The length of the course.
-   */
-  set length(value) {
-    if (typeof value !== 'number') {
-      throw new TypeError('length must be a number');
-    }
-    this._length = value;
-  }
-
-  /**
    * Sets the students in the course.
    * @param {string[]} students - The students in the course.
    */
-  set students(value) {
-    if (!Array.isArray(value)) {
+  set students(students) {
+    if (!Array.isArray(students)) {
       throw new TypeError('students must be an array');
     }
-    if (value.every(student => typeof student !== 'string')) {
+    if (students.some(student => typeof student !== 'string')) {
       throw new TypeError('students must be an array of strings');
     }
     this._students = students;
