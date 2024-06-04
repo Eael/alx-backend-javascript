07304acbe12211fae66e7230f702f5af4b6b344e
Eael <eaelspagad@outlook.com>
2024-06-04 17:21:48 +0000

    Adding new

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,9 @@
 export default function getListStudents() {
-  const students = [
-  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-  { id: 2, firstName: 'James', location: 'Columbia' },
-  { id: 5, firstName: 'Serena', location: 'San Francisco' },
-  ];
-  return students;
-}
+  const res = [];
+  res.push({ id: 1, firstName: 'Guillaume', location: 'San Francisco' });
+  res.push({ id: 2, firstName: 'James', location: 'Columbia' });
+  res.push({ id: 5, firstName: 'Serena', location: 'San Francisco' });
+  
+  return res;
+  }
+  
---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,3 @@
+export default getListStudentIds => {
+  return getListStudentIds.map((student) => student.id);
+}
