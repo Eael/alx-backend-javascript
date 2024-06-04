e06b364f5a00d99203419e8f24f614922db30746
Eael <eaelspagad@outlook.com>
2024-06-04 18:05:45 +0000

    lINTING

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,9 +1,13 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
   if (!(students instanceof Array)) return [];
   if (typeof city !== 'string') return [];
+  const defaultGrade = { studentId: -1, grade: 'N/A' }
 
-  return students.filter(student => student.city === city).map(student => {
-  const grades = newGrades.find(grade => grade.studentId === student.id)
-  return grades ? { ...student, grade: grades.grade } : student
+  return students.map((student) => {
+    const grade = newGrades.find((grade) => grade.studentId === student.id);
+    return {
+      ...student,
+      grade: grade && student.location === city ? grade.grade : defaultGrade.grade
+    }
   })
 }
