1e728a6831c9ad51f75fecd4c6a808dacfe97484
Eael <eaelspagad@outlook.com>
2024-06-04 18:15:46 +0000

    more

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,30 +1,17 @@
-/**
- * Updates the grades of a list of students in a given city.
- * @param {{
-*   id: Number,
-*   firstName: String,
-*   location: String
-* }[]} students - The list of students.
-* @param {*} city - The city of students.
-* @param {{
-*   studentId: Number,
-*   grade: Number,
-* }[]} newGrades - The new grades to be given to a student.
-* @returns {{id: Number, firstName: String, location: String}[]}
-*/
+
 export default function updateStudentGradeByCity(students, city, newGrades) {
   if (!(students instanceof Array)) return [];
   if (typeof city !== 'string') return [];
   const defaultGrade = { grade: 'N/A' }
 
   return students
-    .filter(student => student.location === city)
-    .map(student => ({
+    .filter((student) => student.location === city)
+    .map((student) => ({
       id: student.id,
       firstName: student.firstName,
       location: student.location,
-      grade:( newGrades
-        .find(grade => grade.studentId === student.id)
+      grade: (newGrades
+        .filter((grade) => grade.studentId === student.id)
         .pop() || defaultGrade).grade,
   }));
 }
